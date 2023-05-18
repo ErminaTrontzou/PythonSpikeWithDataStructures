0a689d84d84bc214c422a6d502d4298784cc0982
Ermina Trontzou <7erminatrontzou7@gmail.com>
2023-05-18 20:43:56 +0300

    Modified answer one to handle every possible scenario

--- CardSearch/binary_search.py
+++ CardSearch/binary_search.py
@@ -6,24 +6,30 @@ import math
 # She challenges Bob to pick out the card containing a given number by turning over as few cards as possible.
 # Write a function to help Bob locate the card.
 
+
 #Answer 1:
 #Let's assume that the cards are made up of numbers : cards[20,16,13,10,7,4,2]
 #Let's assume that the correct answer is the card with number 10 (at index 3)
 
 
-def locate_card(cards,cardToFind):
+def locate_card_with_binary_search(cards,card_to_find):
     index = 0
-    while True:
-        if(cards[index] == cardToFind):
+
+    print('Cards:', cards)
+    print('Card to find:', card_to_find)
+
+    while index < len(cards):
+        if(cards[index] == card_to_find):
             return index
         index += 1
-        if(index==len(cards)):
-           return "Card not found"
+    return -1
     
-
 cards=[20,16,13,10,7,4,2]
-cardToFind = 10
+card_to_find = 10
 
-result = locate_card(cards,cardToFind) + 1
-print("The card at location " + str(result) + " is the one.")
+result = locate_card_with_binary_search(cards,card_to_find)
+if(result==-1):
+    print("Card not found)")
+else:
+    print("The card at location " + str(result) + " is the one, given that the list starts at index 0")
 
---
+++ tests/test_binary_search.py
@@ -0,0 +1,78 @@
+#We should create some tests for every possible combination scenario
+import pytest
+
+#Tests for answer one
+from CardSearch.binary_search import locate_card_with_binary_search
+
+#_card_to_find occurs in the middle
+def test_locate_card_with_binary_search_with_card_to_find_at_the_middle():
+    # Given.
+    cards=[13, 11, 10, 7, 4, 3, 1, 0]
+    card_to_find = 1
+    # When.
+    result = locate_card_with_binary_search(cards,card_to_find)
+    # Then.
+    assert result == 6
+
+def test_locate_card_with_binary_search_with_card_to_find_at_the_beginning():
+    # Given.
+    cards=[4, 2, 1, -1]
+    card_to_find = 4
+    # When.
+    result = locate_card_with_binary_search(cards,card_to_find)
+    # Then.
+    assert result == 0
+
+def test_locate_card_with_binary_search_with_card_to_find_at_the_end():
+    # Given.
+    cards=[3, -1, -9, -127]
+    card_to_find = -127
+    # When.
+    result = locate_card_with_binary_search(cards,card_to_find)
+    # Then.
+    assert result == 3
+
+def test_locate_card_with_binary_search_with_only_one_card():
+    # Given.
+    cards=[6]
+    card_to_find = 6
+    # When.
+    result = locate_card_with_binary_search(cards,card_to_find)
+    # Then.
+    assert result == 0
+
+def test_locate_card_with_binary_search_with_card_to_find_not_contained_in_cards():
+    # Given.
+    cards=[9, 7, 5, 2, -9]
+    card_to_find = 3
+    # When.
+    result = locate_card_with_binary_search(cards,card_to_find)
+    # Then.
+    assert result == -1
+
+def test_locate_card_with_binary_search_with_cards_empty():
+    # Given.
+    cards=[]
+    card_to_find = 7
+    # When.
+    result = locate_card_with_binary_search(cards,card_to_find)
+    # Then.
+    assert result == -1
+
+def test_locate_card_with_binary_search_with_repeated_cards_but_only_one_card_to_find():
+    # Given.
+    cards=[8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0]
+    card_to_find = 3
+    # When.
+    result = locate_card_with_binary_search(cards,card_to_find)
+    # Then.
+    assert result == 7
+
+def test_locate_card_with_binary_search_with_repeated_cards_with_desired_card_to_find():
+    # Given.
+    cards=[8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0]
+    card_to_find = 6
+    # When.
+    result = locate_card_with_binary_search(cards,card_to_find)
+    # Then.
+    assert result == 2
